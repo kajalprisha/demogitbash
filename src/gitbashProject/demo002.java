1c0ee269536c48473df8eec10b461a90ba3a8be0
kajalprisha <kajalpagare12@gmail.com>
2022-02-01 14:59:03 +0530

    modified 2nd time

--- src/gitbashProject/demo002.java
+++ src/gitbashProject/demo002.java
@@ -5,6 +5,7 @@ public class demo002 {
 	public static void main(String[] args) {
 		System.out.println("practice demo002");
 		System.out.println("practice demo once");
+		System.out.println("practice demo repeted");
 
 	}
 
